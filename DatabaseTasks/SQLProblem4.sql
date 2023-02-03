6b07c6949def8fdd6e172265f50226024ffcf707
AhmedElazony <ahmedazony2@gmail.com>
2023-02-03 18:46:24 +0200

    making comments on SQL problem files

--- DatabaseTasks/SQLProblem4.sql
+++ DatabaseTasks/SQLProblem4.sql
@@ -1,2 +1,3 @@
+/*Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.*/
 SELECT CITY.NAME FROM CITY
 WHERE COUNTRYCODE = "JPN";
--- DatabaseTasks/SQLProblem5.sql
+++ DatabaseTasks/SQLProblem5.sql
@@ -1,2 +1,3 @@
+/*Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name.*/
 SELECT Name FROM STUDENTS
 WHERE Marks > 75 ORDER BY RIGHT(Name,3), ID ASC;
