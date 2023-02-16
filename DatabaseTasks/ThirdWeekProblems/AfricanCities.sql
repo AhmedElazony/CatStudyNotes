345f68f278e89ec0d22f7247919cde37309c0bde
AhmedElazony <ahmedazony2@gmail.com>
2023-02-16 18:31:21 +0200

    Adding problems files

---
+++ DatabaseTasks/ThirdWeekProblems/AfricanCities.sql
@@ -0,0 +1,6 @@
+/*Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.*/
+
+SELECT NAME FROM CITY
+WHERE COUNTRYCODE IN (
+    SELECT CODE FROM COUNTRY
+	WHERE CONTINENT = 'Africa');
